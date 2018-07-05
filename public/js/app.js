6f4aed8b9454d81d436b3f4cb19a20ad3fc22f03
Jean Santos <jeansantosc@gmail.com>
2018-07-05 01:05:46 -0300

    add:client side  router declaration | styles on active | navigate to clicked url

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,56 @@ window.addEventListener('load', () => {
   const exchangeTemplate = Handlebars.compile($('#exchange-template').html());
   const historicalTemplate = Handlebars.compile($('#historical-template').html());
 
-  const html = ratesTemplate();
-  el.html(html);
+  // router declaration
+  const router = new Router({ // Router declared in index.html scripts tag
+    mode: 'history',
+    page404: (path) => {
+      const html = errorTemplate({
+        color: 'yellow',
+        title: 'Error 404 - Page NOT Found!',
+        message: `The path '/${path}' does not exist on this site`
+      });
+
+      el.html(html);
+    }
+  });
+
+  router.add('/', () => {
+    let html = ratesTemplate();
+    el.html(html);
+  });
+
+  router.add('/exchange', () => {
+    let html = exchangeTemplate();
+    el.html(html);
+  });
+
+  router.add('/historical', () => {
+    let html = historicalTemplate();
+    el.html(html);
+  });
+
+  // navigate app to current url
+  router.navigateTo(window.location.pathname);
+
+  // highlight active menu on refresh/page load
+  const link =  $(`a[href$='${window.location.pathname}']`);
+  link.addClass('active');
+
+  $('a').on('click', (event) => {
+    // block browser page load
+    event.preventDefault();
+
+    // highlight active menu on click
+    const target = $(event.target);
+    $('.item').removeClass('active');
+    target.addClass('active');
+
+    // navigate to clicked url
+    const href = target.attr('href');
+    const path = href.substr(href.lastIndexOf('/'));
+    console.log(path);
+    router.navigateTo(path);
+  });
+
 });
--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(express.static('public'));
 // allow front-end access to node_modules
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
+// redirect all traffic to index.html - since its a one page website, makes sense
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
 // listen for http request on port 3000
 app.listen(port, () => {
   console.log('listening on %d', port);
